7489b3a444670a7187aaf5aebea96a618a995711
DeValmeida <79146033+valber4513@users.noreply.github.com>
2021-10-12 21:40:10 -0300

    Mini Sistema de calculos de notas. Usuário coloca seu Login e senha  *  depois entra com seus dados pessoais, no final entra com as matérias e suas notas  *  calculando e trazendo a situação Final aprovado, reprovado ou em recuperação */

---
+++ MiniProjetoCalculo/src/sistema/Disciplinas.java
@@ -0,0 +1,48 @@
+package sistema;
+
+import java.util.Objects;
+
+public class Disciplinas {
+
+	public double nota;
+	public String disciplina;
+
+	public String toString() {
+		return "Disciplinas [nota=" + nota + ", disciplina=" + disciplina + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(disciplina, nota);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disciplinas other = (Disciplinas) obj;
+		return Objects.equals(disciplina, other.disciplina)
+				&& Double.doubleToLongBits(nota) == Double.doubleToLongBits(other.nota);
+	}
+
+	public double getNota() {
+		return nota;
+	}
+
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+
+	public String getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+
+}
---
+++ MiniProjetoCalculo/src/sistema/materias.java
@@ -0,0 +1,19 @@
+package sistema;
+
+public class materias {
+
+	public static void main(String[] args) {
+
+		
+		
+
+		
+}
+	public String Disciplina;
+	public String getDisciplina() {
+		return Disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		Disciplina = disciplina;
+	}
+}
